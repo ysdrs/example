d66940976310ed2f9eac18f3a18f822aca7aa1fc
柠檬冲水 <916415899@qq.com>
2024-07-09 22:46:35 +0800

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,13 @@
-import fs from 'node:fs'
-const files = fs.readdirSync('./plugins/example/apps').filter(file => /(.js|.ts)$/.test(file))
+import { readdirSync } from 'node:fs'
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const appsDir = join(__dirname, 'apps');
+// 写入不想要的文件
+const IncludeFile = []
+// 如不想要添加表情   const IncludeFile = [' add.ts']
+const files = readdirSync(appsDir).filter(file => (!IncludeFile.includes(file) && (/(.js|.ts)$/.test(file))))
 const arr = []
 files.forEach((file) => {
   arr.push(import(`./apps/${file}`))
@@ -9,8 +17,9 @@ const apps = {}
 for (const i in files) {
   // 得到插件名
   const name = files[i].replace(/(.js|.ts)/, '')
+  //
   if (ret[i].status !== 'fulfilled') {
-    logger.error(`载入插件错误：${logger.red(name)}`)
+    logger.error(`载入插件错误：${logger.chalk.red(name)}`)
     continue
   }
   const key = Object.keys(ret[i].value)[0]
